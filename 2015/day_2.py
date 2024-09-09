e2662af030c35b81603a6a037ccb4027426af1cf
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-09-09 14:48:05 +0100

    Day 2 done

--- 2015/day_2.py
+++ 2015/day_2.py
@@ -37,13 +37,29 @@ def calculate_volume(dimensions:list[str]) -> int:
 
 
 def calculate_around_present(dimensions: list[str]) -> int:
-    """Calculates the shortest distance around a present. """
-    dimensions.sort()
-    return (2 * int(dimensions[0]) + 2 * int(dimensions[1]))
+    """Calculates the shortest distance around a present."""
+    sorted_dimensions = sorted(
+        dimensions, key=int)  # Sort dimensions by integer size
+    return 2 * int(sorted_dimensions[0]) + 2 * int(sorted_dimensions[1])
+
+
+def get_total_ribbon(data: list[str]) -> int:
+    """Calculates the total amount of ribbon needed."""
+    total_ribbon = 0
+
+    for i in data:
+        strings_list = convert_to_str_list(i)
+        total_ribbon += calculate_volume(strings_list)
+        total_ribbon += calculate_around_present(strings_list)
+    
+    return total_ribbon
 
 if __name__ == "__main__":
     file = "day_2_data.txt"
     data = read_input(file)
-    # print(convert_to_str_list("29x13x26"))
+
     amount = get_total_paper(data)
-    print(f"The elves need {amount} square feet of wrapping paper.")
+    print(f"Part 1: The elves need {amount} square feet of wrapping paper.")
+
+    ribbon = get_total_ribbon(data)
+    print(f"Part 2: The elves need {ribbon} feet of ribbon.")
