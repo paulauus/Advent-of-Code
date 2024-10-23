1c663ae05979bf1e11c9b654b1e4ab38624ecd9a
Paula <paula.uusnakki@gmail.com>
2024-10-23 17:25:42 +0100

    Day 3 part 2

--- 2016/day_3.py
+++ 2016/day_3.py
@@ -19,7 +19,43 @@ def get_is_triangle(data: list[str]) -> int:
 
     return triangle_count
 
+def create_vertical_triangles(data: list[str]) -> list[list]:
+    """Returns a list of new triangles."""
+    new_list = []
+    # Creates a list of lists
+    for line in data:
+        new_list.append(line.split())
+
+    vertical_list = []
+    for i in range(len(new_list)):
+        if i % 3 == 0:
+            vertical_list.append(
+                [int(new_list[i][0]), int(new_list[i+1][0]), int(new_list[i+2][0])])
+            vertical_list.append(
+                [int(new_list[i][1]), int(new_list[i+1][1]), int(new_list[i+2][1])])
+            vertical_list.append(
+                [int(new_list[i][2]), int(new_list[i+1][2]), int(new_list[i+2][2])])
+
+    return vertical_list
+
+
+def get_is_triangle_vertical(data: list[list]) -> int:
+    """Returns the number of valid triangles in the new list."""
+    triangle_count = 0
+    for line in data:
+        if (line[0] + line[1] > line[2]) and (
+            line[1] + line[2] > line[0]) and (
+                line[2] + line[0] > line[1]):
+            triangle_count += 1
+
+    return triangle_count
+
 if __name__ == "__main__":
+    # Part 1
     readings = read_input("day_3_data.txt")
     answer_1 = get_is_triangle(readings)
     print(f"Part 1: {answer_1}")
+    # Part 2
+    vertical_triangles = create_vertical_triangles(readings)
+    answer_2 = get_is_triangle_vertical(vertical_triangles)
+    print(f"Part 2: {answer_2}")
