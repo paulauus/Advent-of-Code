4dc268341bac830b09010f6bfddcb4067de2c027
Paula <paula.uusnakki@gmail.com>
2024-10-24 12:24:05 +0100

    Linting fixes

--- 2017/day_2.py
+++ 2017/day_2.py
@@ -6,13 +6,14 @@ def read_input(filename: str) -> list[str]:
     """Reads a text file into a str."""
     with open(filename, "r", encoding="UTF-8") as f:
         data = f.readlines()
-    
+
     # Split strings and create a list of lists
     new_list = []
     for line in data:
         new_list.append(line.split())
 
     return new_list
+
 
 def calculate_checksum(data: list[list]) -> int:
     """Calculates the checksum in the puzzle input."""
@@ -23,10 +24,8 @@ def calculate_checksum(data: list[list]) -> int:
         # Find max and min numbers
         for num in line:
             num = int(num)  # Change str to int
-            if num < min_number:
-                min_number = num
-            if num > max_number:
-                max_number = num
+            min_number = min(min_number, num)
+            max_number = max(max_number, num)
         # Find difference between max and min
         diff_number = max_number - min_number
         # Add difference to total
@@ -34,7 +33,8 @@ def calculate_checksum(data: list[list]) -> int:
 
     return checksum
 
+
 if __name__ == "__main__":
-    data = read_input("day_2_data.txt")
-    answer_1 = calculate_checksum(data)
+    num_list = read_input("day_2_data.txt")
+    answer_1 = calculate_checksum(num_list)
     print(f"Part 1: {answer_1}")
