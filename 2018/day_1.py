9627499f3fc551e3dcb27466ac76b9b8cfd98dfc
Paula <paula.uusnakki@gmail.com>
2024-10-29 16:41:38 +0000

    Day 1 part 2

--- 2018/day_1.py
+++ 2018/day_1.py
@@ -21,18 +21,19 @@ def calculate_frequency_change(data: list) -> int:
 
 
 def find_repeating_frequency(data: list) -> int:
-    """Calculates the resulting frequency after all changes."""
+    """Finds the first frequency reached twice."""
     frequency = 0
     frequency_list = []
-    for change in data:
-        a, b = change[0], int(change[1:])
-        if a == "+":
-            frequency += b
-        else:
-            frequency -= b
-        frequency_list.append(frequency)
-        if frequency in frequency_list:
-            return frequency
+    while True:
+        for change in data:
+            a, b = change[0], int(change[1:])
+            if a == "+":
+                frequency += b
+            else:
+                frequency -= b
+            if frequency in frequency_list:
+                return frequency
+            frequency_list.append(frequency)
 
 
 if __name__ == "__main__":
@@ -40,3 +41,6 @@ if __name__ == "__main__":
     # Part 1
     answer_1 = calculate_frequency_change(readings)
     print(f"Part 1: {answer_1}")
+    # Part 2
+    answer_2 = find_repeating_frequency(readings)
+    print(f"Part 2: {answer_2}")
