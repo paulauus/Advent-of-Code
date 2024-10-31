b5042897df3f6c2ab3c7538825be6721ea20f52a
Paula <paula.uusnakki@gmail.com>
2024-10-31 17:18:50 +0000

    First function done

--- 2018/day_2.py
+++ 2018/day_2.py
@@ -1,5 +1,7 @@
 """Day 2: Inventory Management System"""
 
+from thefuzz import fuzz
+
 
 def read_input(filename: str) -> list[str]:
     """Reads a text file into a list."""
@@ -25,8 +27,20 @@ def calculate_checksum(boxes: list[str]) -> int:
 
     return two_counts * three_counts
 
+def find_similar_boxes(boxes: list[str]) -> list[str]:
+    """Finds the two words where one letter is different."""
+    similar_boxes = []
+    similarity = int((len(boxes[0])-1)/len(boxes[0]) * 100)
+    for a in boxes:
+        for b in boxes:
+            if fuzz.ratio(a, b) == similarity:
+                similar_boxes.append(a)
+
+    return similar_boxes
+
 
 if __name__ == "__main__":
     data = read_input("day_2_data.txt")
     answer_1 = calculate_checksum(data)
     print(f"Part 1: {answer_1}")
+    print(find_similar_boxes(data))
