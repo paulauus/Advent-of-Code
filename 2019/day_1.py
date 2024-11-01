56f2ef7d9a1428c7d5d59dd11ac26e7379b8bfca
Paula <paula.uusnakki@gmail.com>
2024-11-01 15:45:04 +0000

    Day 1 part 1

--- 2019/day_1.py
+++ 2019/day_1.py
@@ -1,1 +1,23 @@
 """Day 1: The Tyranny of the Rocket Equation"""
+
+import math
+
+
+def read_input(filename: str) -> list[str]:
+    """Reads a text file into a list of strings."""
+    with open(filename, "r", encoding="UTF-8") as f:
+        return f.readlines()
+
+
+def calculate_fuel_requirement(modules: list[str]) -> int:
+    """Calculates the sum of the fuel requirements for all of the modules."""
+    fuel_requirement = 0
+    for module in modules:
+        fuel_requirement += (math.floor(int(module) / 3) - 2)
+
+    return fuel_requirement
+
+if __name__ == "__main__":
+    data = read_input("day_1_data.txt")
+    answer_1 = calculate_fuel_requirement(data)
+    print(f"Part 1: {answer_1}")
