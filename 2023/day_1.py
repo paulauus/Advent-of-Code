8017bebe85daabce3f778e91aba46922f60aa699
Paula <paula.uusnakki@gmail.com>
2024-11-28 14:17:10 +0000

    Readlines

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -1,4 +1,10 @@
 """Day 1: Trebuchet?!"""
 
+def read_input(filename: str) -> list[str]:
+    """Reads the .txt file into a list of strings."""
+    with open(filename, "r", encoding="UTF-8") as f:
+        return f.readlines()
+
 if __name__ == "__main__":
-    ...
+    data = read_input("day_1_data.txt")
+    print(data)
