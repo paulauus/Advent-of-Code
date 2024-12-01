82dfd7fe0c80b6975302f3e8d36290a20198fbff
Paula <paula.uusnakki@gmail.com>
2024-12-01 21:52:56 +0000

    Day 1 part 2

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -6,7 +6,7 @@ def read_input(filename: str) -> list[str]:
     with open(filename, "r", encoding="UTF-8") as f:
 
         return f.readlines()
-    
+
 
 def get_sorted_list(locations: list[str]) -> list[list[int]]:
     """Returns a list with two sorted lists of integers."""
@@ -32,8 +32,23 @@ def find_distances(sorted_list: list[list[int]]) -> int:
     return total_distance
 
 
+def calculate_similarity_score(sorted_list: list[list[int]]) -> int:
+    """
+    Adds up each number in the left list after multiplying it 
+    by the number of times that number appears in the right list.
+    """
+    similarity_score = 0
+
+    for num in sorted_list[0]:
+        similarity_score += num * sorted_list[1].count(num)
+
+    return similarity_score
+
 if __name__ == "__main__":
     data = read_input("day_1_data.txt")
     # Part 1
     answer_1 = find_distances(get_sorted_list(data))
     print(f"Part 1: {answer_1}")
+    # Part 2
+    answer_2 = calculate_similarity_score(get_sorted_list(data))
+    print(f"Part 2: {answer_2}")
