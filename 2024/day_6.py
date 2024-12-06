cd1a4733ede10f296b6cf90736d738d84770d99c
Paula <paula.uusnakki@gmail.com>
2024-12-06 16:53:43 +0000

    Day 6 part 1

--- 2024/day_6.py
+++ 2024/day_6.py
@@ -4,7 +4,7 @@ def read_lines(filename: str) -> list[str]:
     """Reads the .txt file into a list of strings."""
     with open(filename, "r", encoding="UTF-8") as f:
         return [line.strip() for line in f.readlines()]
-    
+
 
 def find_distinct_positions(mapped_area: list[str]) -> int:
     """Returns the number of distinct positions visited by the guard."""
@@ -16,7 +16,7 @@ def find_distinct_positions(mapped_area: list[str]) -> int:
         "down": (0, 1),
         "left": (-1, 0)
     }
-    
+
     for i, row in enumerate(mapped_area):
         for j, spot in enumerate(row):
             if spot == "^":
@@ -29,7 +29,7 @@ def find_distinct_positions(mapped_area: list[str]) -> int:
     while True:
         next_x = x + moves[directions[direction]][0]
         next_y = y + moves[directions[direction]][1]
-        
+
         if next_x not in range(len(mapped_area[0])) or next_y not in range(len(mapped_area)):
             break
 
@@ -44,7 +44,7 @@ def find_distinct_positions(mapped_area: list[str]) -> int:
 
     return len(distinct_positions)
 
-    
+
 if __name__ == "__main__":
     data = read_lines("day_6_data.txt")
     print(find_distinct_positions(data))
