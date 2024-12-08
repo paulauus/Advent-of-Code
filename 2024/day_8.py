2b9c4f7c5624d81012aa49f2595c045abb82df90
Paula <paula.uusnakki@gmail.com>
2024-12-08 18:59:06 +0000

    Correct answer

--- 2024/day_8.py
+++ 2024/day_8.py
@@ -5,6 +5,41 @@ def read_lines(filename: str) -> list[str]:
     with open(filename, "r", encoding="UTF-8") as f:
         return [line.strip() for line in f.readlines()]
     
+
+def get_antenna_coordinates(map: list[str]) -> list[list[int|str]]:
+    """Returns all antennas with coordinates."""
+    coordinates = []
+
+    for y, line in enumerate(map):
+        for x, item in enumerate(line):
+            if item != ".":
+                coordinates.append([item, int(x), int(y)])
+
+    return coordinates
+
+
+def find_unique_antinode_locations(coordinates: list[list[int | str]], map: list[str]):
+    """Returns the number of unique locations that contain an antinode."""
+    antinodes = []
+
+    for i, antenna in enumerate(coordinates):
+
+        for match in coordinates[i+1:]:
+
+            if match[0] == antenna[0]:
+                x_diff = match[1] - antenna[1]
+                y_diff = match[2] - antenna[2]
+                antinode_x1, antinode_y1 = antenna[1] - x_diff, antenna[2] - y_diff
+                if 0 <= antinode_x1 < len(map[0]) and 0 <= antinode_y1 < len(map) and (antinode_x1, antinode_y1) not in antinodes:
+                    antinodes.append((antinode_x1, antinode_y1))
+                antinode_x2, antinode_y2 = match[1] + x_diff, match[2] + y_diff
+                if 0 <= antinode_x2 < len(map[0]) and 0 <= antinode_y2 < len(map) and (antinode_x2, antinode_y2) not in antinodes:
+                    antinodes.append((antinode_x2, antinode_y2))
+
+    return len(antinodes)
+    
 if __name__ == "__main__":
     data = read_lines("day_8_data.txt")
-    print(data)
+    coordinates = get_antenna_coordinates(data)
+    answer_1 = find_unique_antinode_locations(coordinates, data)
+    print(answer_1)
