827d9320384da40ee28715c40a590cfc7367ddab
Paula <paula.uusnakki@gmail.com>
2024-12-08 19:56:11 +0000

    Correct answer 2

--- 2024/day_8.py
+++ 2024/day_8.py
@@ -40,9 +40,43 @@ def find_unique_antinode_locations(coordinates: list[list[int | str]], grid: lis
 
     return len(antinodes)
 
+
+def find_updated_locations(coordinates: list[list[int | str]], grid: list[str]):
+    """Returns the number of antinodes using the updated model."""
+    antinodes = set()
+
+    for i, antenna in enumerate(coordinates):
+        # Add the antenna itself if there are any matches, skip itself
+        if any(match[0] == antenna[0] for match in coordinates if match != antenna):
+            antinodes.add((antenna[1], antenna[2]))
+
+        for match in coordinates[i+1:]:
+            if match[0] == antenna[0]:  # Same char
+                x_diff = match[1] - antenna[1]
+                y_diff = match[2] - antenna[2]
+
+                # Line towards one direction (from `antenna` backwards)
+                x, y = antenna[1] - x_diff, antenna[2] - y_diff
+                while 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+                    antinodes.add((x, y))
+                    x -= x_diff
+                    y -= y_diff
+
+                # Line towards the other direction (from `match` forward)
+                x, y = match[1] + x_diff, match[2] + y_diff
+                while 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+                    antinodes.add((x, y))
+                    x += x_diff
+                    y += y_diff
+
+    return len(antinodes)
+
 if __name__ == "__main__":
     data = read_lines("day_8_data.txt")
     antenna_coordinates = get_antenna_coordinates(data)
     # Part 1
     answer_1 = find_unique_antinode_locations(antenna_coordinates, data)
     print(f"Part 1: {answer_1}")
+    # Part 2
+    answer_2 = find_updated_locations(antenna_coordinates, data)
+    print(f"Part2: {answer_2}")
