dfc1ee629429d3f402b9b0c3e87deb2e8cae0499
Paula <paula.uusnakki@gmail.com>
2024-12-11 18:15:16 +0000

    Day 11 part 1

--- 2024/day_11.py
+++ 2024/day_11.py
@@ -1,11 +1,43 @@
 """Day 11: Plutonian Pebbles"""
 
-def read_input(filename: str) -> list[str]:
-    """Reads the .txt input into a list of strings."""
-    with open(filename, "r", encoding="UTF-8") as f:
-        return f.read().split()
-    
+from functools import cache
+
+def read_input(filename: str) -> list[int]:
+    """Reads the .txt input into a list of integers."""
+    return list(map(int, open(filename, 'r').read().strip().split()))
+
+@cache
+def corresponding_value(value: int):
+    length = len(str(value))
+    if value == 0:
+        return 1
+
+    elif length % 2 == 0:
+        return [int(str(value)[:length//2]), int(str(value)[length//2:])]
+    else:
+        return value*2024
+
+
+@cache
+def split_amount(number: int, iterations: int) -> int:
+    overall = number
+    splits = 0
+    for i in range(iterations):
+        value = corresponding_value(overall)
+        if isinstance(value, int):
+            overall = value
+        if isinstance(value, list):
+            splits += 1
+            overall = value[0]
+            splits += split_amount(value[1], iterations-i-1)
+    return splits
+
 
 if __name__ == "__main__":
     data = read_input("day_11_data.txt")
-    print(data)
+
+    # Part 1
+    max_iterations = 25
+    answer_1 = (len(data)+sum(split_amount(i, max_iterations)
+          for i in data))
+    print(f"Part 1: {answer_1}")
