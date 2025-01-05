7121887325f732a8532f9be081b1d4fbd213d591
Paula <paula.uusnakki@gmail.com>
2025-01-05 18:39:41 +0000

    Read input

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -1,4 +1,9 @@
 """Day 2: Cube Conundrum"""
 
+def read_input(filename: str) -> list[str]:
+    """Returns a list of strings from a .txt file."""
+    with open(filename, "r", encoding="UTF-8") as f:
+        return f.readlines()
+
 if __name__ == "__main__":
     ...
