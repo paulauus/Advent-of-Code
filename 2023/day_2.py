b248a0b4f84bee33167cb93205f2ca89e7831aea
Paula <paula.uusnakki@gmail.com>
2025-01-05 18:42:38 +0000

    Strip newline

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -3,7 +3,8 @@
 def read_input(filename: str) -> list[str]:
     """Returns a list of strings from a .txt file."""
     with open(filename, "r", encoding="UTF-8") as f:
-        return f.readlines()
+        return [line.strip() for line in f]
 
 if __name__ == "__main__":
-    ...
+    data = read_input("day_2_data.txt")
+    print(data)
