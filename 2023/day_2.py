7ec45aafe9844d9633a404f24d7c0aca5df72877
Paula <paula.uusnakki@gmail.com>
2025-01-05 18:48:14 +0000

    Planned functions

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -4,7 +4,18 @@ def read_input(filename: str) -> list[str]:
     """Returns a list of strings from a .txt file."""
     with open(filename, "r", encoding="UTF-8") as f:
         return [line.strip() for line in f]
+    
+
+def is_game_valid(game: str, VALID_GAME: dict) -> bool:
+    """Checks if a game is valid against given rules."""
+    ...
+
+
+def sum_of_valid_game_ids(all_games: list[str], VALID_GAME: dict) -> int:
+    """Returns the sum of valid game IDs."""
+    ...
 
 if __name__ == "__main__":
     data = read_input("day_2_data.txt")
-    print(data)
+    
+    # Part 1
